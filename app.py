96a0688cddbd2c40bea0a4703d4d6acebcc97088
Shy <Shy@users.noreply.github.com>
2024-02-12 11:48:37 -0500

    playing around with stuff

--- app.py
+++ app.py
@@ -12,10 +12,14 @@ from googleapiclient.errors import HttpError
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from svglib.svglib import svg2rlg
 from reportlab.graphics.renderPM import drawToPIL
-from flask_scheduler import Scheduler
+from flask_apscheduler import APScheduler
 from io import BytesIO
 
 load_dotenv()
+
+
+class Config:
+    SCHEDULER_API_ENABLED = True
 
 
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
@@ -49,6 +53,7 @@ cityUnit = "°F"
 # Load in svg icon. This is from the font awesome free pack. See https://fontawesome.com/
 weather_icon_path = "assets/images/temperature-half.svg"
 cal_icon_path = "assets/images/cal.svg"
+clock_icon_path = "assets/images/clock.svg"
 
 
 def generateScale(low, high, unit, name, icon_path):
@@ -95,15 +100,17 @@ r = requests.post(
     files={"imgfile": weatherScale},
 )
 
-
 app = Flask(__name__)
-scheduler = Scheduler(app)
-app.config["SCHEDULER_API_INTERVAL"] = 60  # in seconds
+app.config.from_object(Config())
+scheduler = APScheduler()
+scheduler.init_app(app)
 
 
-@app.route("/")
-@app.route("/updateWeather")
+# @app.route("/")
+# @app.route("/updateWeather")
+@scheduler.task("interval", id="updateWeather", seconds=120, misfire_grace_time=900)
 def updateWeather():
+    print("Updating Weather")
     r = requests.get(forcastURL)
     data = r.json()
     temp = data["properties"]["periods"][0]["temperature"]
@@ -116,9 +123,15 @@ def updateWeather():
     return r.json()
 
 
-@app.route("/updateEvents")
+# @app.route("/updateEvents")
+@scheduler.task("interval", id="updateEvent", seconds=60, misfire_grace_time=900)
 def updateEvent():
-    calIDs = ['t8c5iip0pbh62sfvjm8nhn2mko5sgn2o@import.calendar.google.com',"primary","shy@hackny.org"]
+    print("Updating Events")
+    calIDs = [
+        "primary",
+        "shy@hackny.org",
+        "t8c5iip0pbh62sfvjm8nhn2mko5sgn2o@import.calendar.google.com",
+    ]
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     try:
@@ -133,7 +146,7 @@ def updateEvent():
                     calendarId=calendarId,
                     timeMin=now,
                     maxResults=2,
-                    eventTypes='default',
+                    eventTypes="default",
                     singleEvents=True,
                     orderBy="startTime",
                 )
@@ -141,45 +154,104 @@ def updateEvent():
             )
             incoming_Event = events_result.get("items", [])
             eventIndex = 0
-            if pendulum.parse(incoming_Event[0]['start']['dateTime']) <= pendulum.now():
+            if pendulum.parse(incoming_Event[0]["start"]["dateTime"]) <= pendulum.now():
                 eventIndex = 1
             if nextEvent:
-                if nextEvent['start']['dateTime'] > incoming_Event[eventIndex]['start']['dateTime']:
+                if (
+                    nextEvent["start"]["dateTime"]
+                    > incoming_Event[eventIndex]["start"]["dateTime"]
+                ):
                     nextEvent = incoming_Event[eventIndex]
             else:
                 nextEvent = incoming_Event[eventIndex]
         if not nextEvent:
             print("No upcoming events found.")
             return 400
-        calScale = generateScale(60, 0, ' Min', f"{nextEvent['summary']}", cal_icon_path)
-        r = requests.post(
-            f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/image/set",
-            params={"key": VU_Key, "imgfile": "my_awesome_image.png"},
-            files={"imgfile": calScale},
-        )
-        eventStart = pendulum.parse(nextEvent['start']['dateTime'])
+
+        eventStart = pendulum.parse(nextEvent["start"]["dateTime"])
         timeRemaining = eventStart.diff(pendulum.now()).in_minutes()
-        if timeRemaining >= 60:
+        print(timeRemaining)
+        if timeRemaining >= 360:
+            # Greater than 6 hours.
+            calScale = generateScale(
+                6, 0, " Hrs", f"{nextEvent['summary']}", cal_icon_path
+            )
+            r = requests.post(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/image/set",
+                params={"key": VU_Key, "imgfile": "my_awesome_image.png"},
+                files={"imgfile": calScale},
+            )
             r = requests.get(
                 f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/set",
                 params={"key": VU_Key, "value": 0},
             )
-        else:
+            r = requests.get(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/backlight",
+                params={"key": VU_Key, "red": 0, "green": 0, "blue": 0},
+            )
+            scheduler.modify_job("updateEvent", trigger="interval", minutes=60)
+        elif timeRemaining >= 60:
+            # Between 6 and 1 hour remain.
+            calScale = generateScale(
+                6, 0, " Hrs", f"{nextEvent['summary']}", cal_icon_path
+            )
+            r = requests.post(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/image/set",
+                params={"key": VU_Key, "imgfile": "my_awesome_image.png"},
+                files={"imgfile": calScale},
+            )
             r = requests.get(
                 f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/set",
-                params={"key": VU_Key, "value": 1-(timeRemaining/60)},
+                params={"key": VU_Key, "value": 100 * (1 - (timeRemaining / 60 / 6))},
             )
-
+            r = requests.get(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/backlight",
+                params={"key": VU_Key, "red": 0, "green": 0, "blue": 0},
+            )
+            scheduler.modify_job("updateEvent", trigger="interval", minutes=15)
+        elif timeRemaining > 10:
+            # Less than an hour, more than 10 minutes.
+            calScale = generateScale(
+                60, 0, " Min", f"{nextEvent['summary']}", clock_icon_path
+            )
+            r = requests.post(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/image/set",
+                params={"key": VU_Key, "imgfile": "my_awesome_image.png"},
+                files={"imgfile": calScale},
+            )
+            r = requests.get(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/set",
+                params={"key": VU_Key, "value": 100 * ((60 - timeRemaining) / 60)},
+            )
+            r = requests.get(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/backlight",
+                params={"key": VU_Key, "red": 0, "green": 0, "blue": 0},
+            )
+            scheduler.modify_job("updateEvent", trigger="interval", minutes=1)
+        else:
+            # 10 minutes or less.
+            calScale = generateScale(
+                60, 0, " Min", f"{nextEvent['summary']}", clock_icon_path
+            )
+            print(100 * ((60 - timeRemaining) / 60))
+            r = requests.get(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/set",
+                params={"key": VU_Key, "value": 100 * ((60 - timeRemaining) / 60)},
+            )
+            r = requests.get(
+                f"http://localhost:5340/api/v0/dial/{cal_dial_UID}/backlight",
+                params={
+                    "key": VU_Key,
+                    "red": 100 * ((60 - timeRemaining) / 60),
+                    "green": 20,
+                    "blue": 40,
+                },
+            )
+            scheduler.modify_job("updateEvent", trigger="interval", seconds=30)
         return r.json()
-
 
     except HttpError as error:
         print(f"An error occurred: {error}")
 
 
-@scheduler.runner()
-def my_task():
-    print("Updating Weather")
-    updateWeather()
-    print("Updating Events")
-    updateEvent()
+scheduler.start()
